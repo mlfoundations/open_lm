dca754c15c0a5ecff04be6e3022659c608053aca
Samir Yitzhak Gadre <sagadre@users.noreply.github.com>
2023-10-09 12:32:31 -0400

    version bump for downstream

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ REQUIREMENTS = _read_reqs("requirements.txt")
 
 setuptools.setup(
     name="open_lm",
-    version="0.0.3",
+    version="0.0.4",
     author=[
         "Suchin Gururangan*",
         "Mitchell Wortsman*",
