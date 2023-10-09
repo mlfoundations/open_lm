bda84c7a1ca18a73a312914c468d2801d3881706
Matt Jordan <mattjordan.mail@gmail.com>
2023-10-09 14:14:38 -0700

    Update eval_openlm_ckpt.py

--- eval/eval_openlm_ckpt.py
+++ eval/eval_openlm_ckpt.py
@@ -1,7 +1,7 @@
 import argparse
 import json
 import time
-import sys; sys.path.append('..')
+import sys
 from typing import List
 import builtins as __builtin__
 from open_lm.model import create_params
