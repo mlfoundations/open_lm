c0199a7059754ee08b6a3ffbcab329f34bd5a969
Achal Dave <achalddave@live.com>
2023-10-12 17:42:36 +0000

    Add mitchell_init

--- open_lm/model.py
+++ open_lm/model.py
@@ -180,16 +180,12 @@ class Block(nn.Module):
                 self.feed_forward.w3.weight, std=std, a=-3 * std, b=3 * std
             )
         elif args.ffn_type == "gelu":
-            # Copied from mosaic-ml default init.
-            torch.nn.init.kaiming_normal_(self._ff_w1.weight, a=0.0, mode="fan_in", nonlinearity="relu")
-            torch.nn.init.kaiming_normal_(self._ff_w2.weight, a=0.0, mode="fan_in", nonlinearity="relu")
-            torch.nn.init.zeros_(self._ff_w1.bias)
-            torch.nn.init.zeros_(self._ff_w2.bias)
-            # Following mosaic, which sets the second linear layer to have _is_residual=True, and
-            # init_div_is_residual=True. See
-            # https://github.com/mosaicml/llm-foundry/blob/bdac4c719ce331c90f512e11207a97a2d67b4c20/llmfoundry/models/utils/param_init_fns.py#L56
-            with torch.no_grad():
-                self._ff_w2.weight.div_(math.sqrt(2 * args.n_layers))
+            std = 1.0 / math.sqrt(args.dim)
+            torch.nn.init.trunc_normal_(self._ff_w1.weight, std=std, a=-3 * std, b=3 * std)
+
+            std = (1.0 / math.sqrt(hidden_dim))
+            std = std / math.sqrt(2 * (layer_id + 1))
+            torch.nn.init.trunc_normal_(self._ff_w2.weight, std=std, a=-3 * std, b=3 * std)
 
     def forward(self, x):
         h = x + self.attention(self.attention_norm(x), is_causal=True)
