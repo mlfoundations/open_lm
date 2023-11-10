a803b1f14df43f7bd258cd401ce3b51418cd11c2
sagadre <syagadre@gmail.com>
2023-11-10 17:01:47 +0000

    tests for loader shape

--- tests/test_dataloader.py
+++ tests/test_dataloader.py
@@ -1,4 +1,7 @@
+import torch
+
 from open_lm.data import get_wds_dataset
+from open_lm.train import sample_chunk
 from tests.shared import MockDataArgs
 
 
@@ -11,3 +14,15 @@ def test_dataloader_no_crash():
         pass
 
     assert True
+
+
+def test_dataloader_shape():
+    # basic test to make sure the datalaoder does not crash
+    args = MockDataArgs()
+    di = get_wds_dataset(args, True)
+
+    batch = next(iter(di.dataloader))
+    (texts,) = batch
+    inputs, targets = sample_chunk(torch.LongTensor(texts), args.seq_len, args.target_mask_left)
+    assert inputs.shape[-1] == args.seq_len
+    assert targets.shape[-1] == args.seq_len
