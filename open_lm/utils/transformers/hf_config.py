e50183d2cdac9944888b8b04ed58ba4f9cc9376c
Rui Xin <rx31@cs.washington.edu>
2024-01-18 15:27:21 -0500

    Update hf_config and hf_model to support HELM eval (#54)
    * feat: make `Params` seralizable
    
    * feat: allow wrapping open_lm models for AutoModelForCausalLM
    
    * fix: from_pretrained should only take one positional argument
    
    * feat: skip creating a symlink when creating a model
    
    * feat: defaults model loading to bfloat16
    
    * fix: use norm type in args
    
    * cleanup: remove obsolete code
    
    * feat: connect open_lm to helm
    
    * lint: run black on all
    
    * feat: add readme for eval
    
    * fix: add printing out executed command
    
    * fix typo
    
    * fix: change readme
    
    * fix bugs and improve readme
    
    * feat: add script that outputs eval metrics
    
    * fix: update hf wrapper to reduce modification to model.py
    
    * fix: fix ci
    
    * fix: add tie_word_embeddings back
    
    * delete files moved to dcnlp repo
    
    * fix ci
    
    * feat: add additional checks to ensure data format
    
    * doc: improve docs and add more checks
    
    * fix: revert check
    
    * doc: change printed comments

--- open_lm/utils/transformers/hf_config.py
+++ open_lm/utils/transformers/hf_config.py
@@ -1,17 +1,45 @@
 from dataclasses import fields
-from typing import List
+from typing import List, Optional, Dict
+from argparse import Namespace
 
 from transformers import PretrainedConfig
 
-from open_lm.model import Params
+from open_lm.model import Params, create_params
 
 
 class OpenLMConfig(PretrainedConfig):
     model_type = "openlm"
 
-    def __init__(self, params: Params, **kwargs):
+    def __init__(
+        self,
+        params: Optional[Params] = None,
+        params_args: Optional[Namespace] = None,
+        params_args_dict: Optional[Dict] = None,
+        **kwargs
+    ):
+        """
+        Initialize the HFConfig class. Any of the three arguments can be used to initialize the class.
+        Note that the instance can get serialized when passing in either params_args or params_args_dict.
+
+        Args:
+            params (Optional[Params]): The parameters object.
+            params_args (Optional[Namespace]): The namespace object containing the parameters arguments.
+            params_args_dict (Optional[Dict]): The dictionary containing the parameters arguments.
+            **kwargs: Additional keyword arguments.
+        """
+
         # Used by huggingface transformers
         super().__init__(**kwargs)
+
+        if params_args is not None:
+            params_args_dict = vars(params_args)
+
+        self.params_args_dict = params_args_dict
+
+        if params is not None:
+            self.params = params
+
+    def set_params(self, params: Params):
         self.tie_word_embeddings = params.weight_tying
         for field in fields(Params):
             setattr(self, field.name, getattr(params, field.name))
