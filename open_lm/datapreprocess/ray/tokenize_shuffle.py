8f20569c2425ea7f8de657d98ec5cd68ed3e63a5
Jeffrey Li <jwl2162@cs.washington.edu>
2024-02-05 17:11:08 -0800

    fix typo

--- open_lm/datapreprocess/ray/tokenize_shuffle.py
+++ open_lm/datapreprocess/ray/tokenize_shuffle.py
@@ -157,7 +157,7 @@ def get_reader(file_type, content_key: str):
 def get_raw_filetype(key: str):
     if key.endswith(".jsonl") or key.endswith(".json"):
         return RawFileType.JSONL
-    elif key.endswith(".jsonl.zst") or key.endswith(".json.zst") or key.endswith(".jsonl.zstd") key.endswith(".json.zstd"):
+    elif key.endswith(".jsonl.zst") or key.endswith(".json.zst") or key.endswith(".jsonl.zstd") or key.endswith(".json.zstd"):
         return RawFileType.ZSTD_JSONL_COMPRESSED
     elif key.endswith(".jsonl.gz") or key.endswith(".json.gz"):
         return RawFileType.GZIP_JSONL_COMPRESSED
