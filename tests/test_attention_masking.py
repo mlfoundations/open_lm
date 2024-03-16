3daf8da1da91e9757dd1fee88fad530ca51f8ae4
Achal Dave <achalddave@live.com>
2024-03-15 17:03:21 -0700

    extra test

--- tests/test_attention_masking.py
+++ tests/test_attention_masking.py
@@ -15,8 +15,17 @@ def test_attention_masking1():
     # Ignore first elements
     attention_mask[:, :4] = 0
 
+    # Run with only last 4 elements of the sequence, no attention mask
+    output_no_mask = xformers_attn(
+        queries[:, 4:],
+        keys[:, 4:],
+        values[:, 4:],
+        is_causal=True,
+        attention_mask=None,
+    )
+
     # Run with only last 4 elements of the sequence
-    output1 = xformers_attn(
+    output_dummy_mask = xformers_attn(
         queries[:, 4:],
         keys[:, 4:],
         values[:, 4:],
@@ -25,9 +34,12 @@ def test_attention_masking1():
     )
 
     # Run with all elements but mask the first 4 elements
-    output2 = xformers_attn(queries, keys, values, is_causal=True, attention_mask=attention_mask)
+    output_mask_initial = xformers_attn(queries, keys, values, is_causal=True, attention_mask=attention_mask)
 
-    output3 = xformers_attn(output2, output2, output2, is_causal=True, attention_mask=attention_mask)
+    output3 = xformers_attn(
+        output_mask_initial, output_mask_initial, output_mask_initial, is_causal=True, attention_mask=attention_mask
+    )
     assert not output3.isnan().any()
 
-    assert torch.allclose(output1, output2[:, 4:])
+    assert torch.allclose(output_no_mask, output_dummy_mask)
+    assert torch.allclose(output_no_mask, output_mask_initial[:, 4:])
