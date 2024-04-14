abc941260a4544442c72c778af4b08c6b0557af8
George Smyrnis <geosmirnis@gmail.com>
2024-04-15 01:01:41 +0200

    Fix torchcompile loading.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ REQUIREMENTS = _read_reqs("requirements.txt")
 
 setuptools.setup(
     name="open_lm",
-    version="0.0.32",
+    version="0.0.33",
     author=[
         "Suchin Gururangan*",
         "Mitchell Wortsman*",
