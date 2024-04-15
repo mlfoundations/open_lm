fc122608bcfd0678dead79c89ea0c0ac1739ea68
Achal Dave <achalddave@gmail.com>
2024-04-15 16:19:48 -0400

    Add attention masking support for torch_attn (#250)
    * Add attention masking support for torch_attn
    
    * Refactor fix

--- tests/test_attention_masking.py
+++ tests/test_attention_masking.py
@@ -1,11 +1,11 @@
 import torch
 import pytest
 
-from open_lm.attention import xformers_attn
+from open_lm.attention import xformers_attn, torch_attn
 
 
 @pytest.mark.gpu
-def test_attention_masking1():
+def test_attention_masking_xformers():
     n, d = 8, 4
     queries = torch.rand((1, n, 1, d)).cuda()
     keys = torch.rand((1, n, 1, d)).cuda()
@@ -37,6 +37,47 @@ def test_attention_masking1():
     output_mask_initial = xformers_attn(queries, keys, values, is_causal=True, attention_mask=attention_mask)
 
     output3 = xformers_attn(
+        output_mask_initial, output_mask_initial, output_mask_initial, is_causal=True, attention_mask=attention_mask
+    )
+    assert not output3.isnan().any()
+
+    assert torch.allclose(output_no_mask, output_dummy_mask)
+    assert torch.allclose(output_no_mask, output_mask_initial[:, 4:])
+
+
+@pytest.mark.gpu
+def test_attention_masking_torchattn():
+    n, d = 8, 4
+    queries = torch.rand((1, n, 1, d))
+    keys = torch.rand((1, n, 1, d))
+    values = torch.rand((1, n, 1, d))
+
+    attention_mask = torch.ones((1, n))
+    # Ignore first elements
+    attention_mask[:, :4] = 0
+
+    # Run with only last 4 elements of the sequence, no attention mask
+    output_no_mask = torch_attn(
+        queries[:, 4:],
+        keys[:, 4:],
+        values[:, 4:],
+        is_causal=True,
+        attention_mask=None,
+    )
+
+    # Run with only last 4 elements of the sequence
+    output_dummy_mask = torch_attn(
+        queries[:, 4:],
+        keys[:, 4:],
+        values[:, 4:],
+        is_causal=True,
+        attention_mask=attention_mask[:, 4:],
+    )
+
+    # Run with all elements but mask the first 4 elements
+    output_mask_initial = torch_attn(queries, keys, values, is_causal=True, attention_mask=attention_mask)
+
+    output3 = torch_attn(
         output_mask_initial, output_mask_initial, output_mask_initial, is_causal=True, attention_mask=attention_mask
     )
     assert not output3.isnan().any()
