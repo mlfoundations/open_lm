616fe381a05b24e6a036a78661c933afb188a84b
Romil Shah <shah.romil@northeastern.edu>
2024-04-17 01:49:46 -0700

    Update model

--- open_lm/model.py
+++ open_lm/model.py
@@ -525,7 +525,7 @@ def nn_linear_to_te_linear(model, include_modules=[], exclude_modules=["output"]
             elif "F.layer_norm" in source_code:
                 print(f"[FP8 TESTS] ----- F.layer_norm found: {name} -----")
                 layer_norm_module = te.LayerNorm(
-                    module.normalized_shape, eps=module.eps, device="cuda", params_dtype=module.input.dtype
+                    module.normalized_shape, eps=module.eps, device="cuda", params_dtype=torch.get_autocast_gpu_dtype()
                 )
                 output_tensor = layer_norm_module(module.input)
                 if module.weight is not None and module.bias is not None:
