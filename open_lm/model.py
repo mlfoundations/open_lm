3996c5a43f50fd566894bec1a0601ae792de8fdf
Romil Shah <shah.romil@northeastern.edu>
2024-04-17 01:35:00 -0700

    Update model.py

--- open_lm/model.py
+++ open_lm/model.py
@@ -523,14 +523,13 @@ def nn_linear_to_te_linear(model, include_modules=[], exclude_modules=["output"]
             if "F.scaled_dot_product_attention" in source_code:
                 print(f"[FP8 TESTS] ----- F.scaled_dot_product_attention found: {name} -----")
             elif "F.layer_norm" in source_code:
-                # layer_norm_module = te.LayerNorm(
-                #     module.normalized_shape, eps=module.eps, device="cuda", params_dtype= module.weight.dtype
-                # )
-                # output_tensor = layer_norm_module(input)
-                # if module.weight is not None and module.bias is not None:
-                #     output_tensor = output_tensor * module.weight + module.bias
-                # model._modules[name] = output_tensor
-                print(f"[FP8 TESTS] ----- F.layer_norm found: {name} -----")
+                layer_norm_module = te.LayerNorm(
+                    module.normalized_shape, eps=module.eps, device="cuda", params_dtype= torch.get_autocast_gpu_dtype()
+                )
+                output_tensor = layer_norm_module(input)
+                if module.weight is not None and module.bias is not None:
+                    output_tensor = output_tensor * module.weight + module.bias
+                model._modules[name] = output_tensor
             elif "te.DotProductAttention" in source_code:
                 print(f"[FP8 TESTS] ----- te.DotProductAttention found: {name} -----")
             elif "te.LayerNorm" in source_code:
