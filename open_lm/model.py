1fd18c036adcfcf453a23e10131c4e66ce2fff79
Romil Shah <shah.romil@northeastern.edu>
2024-04-19 23:08:23 -0700

    Update model

--- open_lm/model.py
+++ open_lm/model.py
@@ -216,9 +216,9 @@ class GemmaMLP(nn.Module):
         super().__init__()
         self.dim = dim
         self.hidden_dim = hidden_dim
-        self.gate_proj = LinearLayerType(dim, hidden_dim, device="cuda")
-        self.up_proj = LinearLayerType(dim, hidden_dim, device="cuda")
-        self.down_proj = LinearLayerType(hidden_dim, dim, device="cuda")
+        self.gate_proj = nn.Linear(dim, hidden_dim, device="cuda")
+        self.up_proj = nn.Linear(dim, hidden_dim, device="cuda")
+        self.down_proj = nn.Linear(hidden_dim, dim, device="cuda")
         self._layer_id = layer_id
 
     def forward(self, x):
