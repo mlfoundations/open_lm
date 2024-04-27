9d43240f1f1b3ea925ccbd1cd764ca6812478082
George Smyrnis <gsmyrnis@utexas.edu>
2024-04-27 17:16:05 -0500

    Fix shapes.

--- open_lm/train.py
+++ open_lm/train.py
@@ -129,16 +129,18 @@ def train_one_epoch(
                 if ref_model is not None and step >= args.rho1_switch * total_steps:
                     ref_out, _, _ = ref_model(inputs)
                     with torch.no_grad():
-                        loss_cur = F.cross_entropy(out.reshape(-1, args.vocab_size), targets.reshape(-1))
-                        loss_ref = F.cross_entropy(ref_out.reshape(-1, args.vocab_size), targets.reshape(-1))
+                        loss_cur = F.cross_entropy(out.reshape(-1, args.vocab_size), targets.reshape(-1), reduction="none")
+                        loss_ref = F.cross_entropy(ref_out.reshape(-1, args.vocab_size), targets.reshape(-1), reduction="none")
                         loss_diff = loss_cur - loss_ref
-                        ref_mask = (loss_diff >= torch.topk(loss_diff, int(round(args.rho1_k * loss_diff.shape[0]))))
-                        loss_targets = targets.reshape(-1).detach().clone()
-                        loss_targets[ref_mask] = -100  # This value is ignored by the loss
+                        ref_mask = (loss_diff < torch.topk(loss_diff, int(round(args.rho1_k * loss_diff.shape[0])))[0][-1])
+                        loss_diff = loss_diff.reshape(inputs_ii.shape[0], inputs_ii.shape[1])
+                        ref_mask = (loss_diff < torch.topk(loss_diff, int(np.ceil(args.rho1_k * loss_diff.shape[0])), dim = -1)[0][-1].reshape(loss_diff.shape[0], 1))
+                        loss_targets = targets_ii.detach().clone()
+                        loss_targets[ref_mask] = -100 # This index is ignored from the loss
                 else:
-                    loss_targets = targets.reshape(-1)
+                    loss_targets = targets
 
-                total_lm_loss = loss(out.reshape(-1, args.vocab_size), loss_targets)
+                total_lm_loss = loss(out.reshape(-1, args.vocab_size), loss_targets.reshape(-1))
                 total_loss = total_lm_loss
                 if args.moe_freq > 0:
                     total_load_balancing_loss = batched_load_balancing_loss(moe_args)
@@ -179,17 +181,18 @@ def train_one_epoch(
                         if ref_model is not None and step >= args.rho1_switch * total_steps:
                             ref_out_ii, _, _ = ref_model(inputs_ii)
                             with torch.no_grad():
-                                loss_cur = F.cross_entropy(out.reshape(-1, args.vocab_size), targets.reshape(-1))
-                                loss_ref = F.cross_entropy(ref_out_ii.reshape(-1, args.vocab_size), targets.reshape(-1))
+                                loss_cur = F.cross_entropy(out.reshape(-1, args.vocab_size), targets_ii.reshape(-1), reduction="none")
+                                loss_ref = F.cross_entropy(ref_out_ii.reshape(-1, args.vocab_size), targets_ii.reshape(-1), reduction="none")
                                 loss_diff = loss_cur - loss_ref
-                                ref_mask = (loss_diff >= torch.topk(loss_diff, int(round(args.rho1_k * loss_diff.shape[0])))).detach().clone()
-                                loss_targets = targets_ii.reshape(-1).detach().clone()
+                                loss_diff = loss_diff.reshape(inputs_ii.shape[0], inputs_ii.shape[1])
+                                ref_mask = (loss_diff < torch.topk(loss_diff, int(np.ceil(args.rho1_k * loss_diff.shape[0])), dim = -1)[0][-1].reshape(loss_diff.shape[0], 1))
+                                loss_targets = targets_ii.detach().clone()
                                 loss_targets[ref_mask] = -100 # This index is ignored from the loss
                         else:
-                            loss_targets = targets_ii.reshape(-1)
+                            loss_targets = targets_ii
                         
                         local_lm_loss = (
-                            loss(out.reshape(-1, args.vocab_size), loss_targets)
+                            loss(out.reshape(-1, args.vocab_size), loss_targets.reshape(-1))
                             * inputs_ii.shape[0]
                             / inputs.shape[0]
                         )
