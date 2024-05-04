509417d192e5d0592147dc7b32f197c938cba0bc
Romil Shah <shah.romil@northeastern.edu>
2024-05-03 22:50:28 -0700

    amax_compute_algo

--- open_lm/train.py
+++ open_lm/train.py
@@ -35,7 +35,7 @@ try:
     from transformer_engine.common import recipe
 
     fp8_format = recipe.Format.HYBRID
-    fp8_recipe = recipe.DelayedScaling(fp8_format=fp8_format, amax_history_len=16, amax_compute_algo="most_recent")
+    fp8_recipe = recipe.DelayedScaling(fp8_format=fp8_format, amax_history_len=16, amax_compute_algo="max")
     using_te = True
 except ImportError as ie:
     using_te = False
