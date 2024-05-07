70ff506cf7a3c7ce18f84e080b09fc07327d14f6
George Smyrnis <gsmyrnis@utexas.edu>
2024-05-06 23:58:19 -0500

    Formatting.

--- open_lm/file_utils.py
+++ open_lm/file_utils.py
@@ -76,7 +76,7 @@ def remote_sync_with_expon_backoff(sync_every, local_dir, remote_dir, protocol, 
         success = remote_sync(local_dir, remote_dir, protocol)
         if success:
             return True
-        
+
     return False
 
 
