0164bacb0ed1a1cab48725be731653b27d329ae7
Jean Mercat <jean.mercat@tri.global>
2024-07-31 10:56:47 -0700

    linted

--- open_lm/utils/llm_foundry_wrapper.py
+++ open_lm/utils/llm_foundry_wrapper.py
@@ -53,29 +53,31 @@ class SimpleComposerOpenLMCausalLM(HuggingFaceModel):
 
     def generate(self, input_ids=None, inputs_embeds=None, **kwargs):
         return super().generate(input_ids=input_ids, **kwargs)
-    
+
     def eval_forward(self, batch, outputs: Optional[Any] = None):
         # If input_ids are all 0 after a certain point, we can skip that
         num_zeros = 0
-        if batch['input_ids'] is not None:
+        if batch["input_ids"] is not None:
             # Find indices of the last non-zero element in each row
-            last_nonzero_indices = torch.max(torch.nonzero(batch['input_ids'], as_tuple=True)[1])
-            if last_nonzero_indices < batch['input_ids'].shape[1] - 1:
-                num_zeros = batch['input_ids'].shape[1] - last_nonzero_indices - 1
-                batch['input_ids'] = batch['input_ids'][:, : last_nonzero_indices + 1]
-                if batch['attention_mask'] is not None:
-                    batch['attention_mask'] = batch['attention_mask'][:, : last_nonzero_indices + 1]
-                if batch['labels'] is not None:
-                    batch['labels'] = batch['labels'][:, : last_nonzero_indices + 1]
+            last_nonzero_indices = torch.max(torch.nonzero(batch["input_ids"], as_tuple=True)[1])
+            if last_nonzero_indices < batch["input_ids"].shape[1] - 1:
+                num_zeros = batch["input_ids"].shape[1] - last_nonzero_indices - 1
+                batch["input_ids"] = batch["input_ids"][:, : last_nonzero_indices + 1]
+                if batch["attention_mask"] is not None:
+                    batch["attention_mask"] = batch["attention_mask"][:, : last_nonzero_indices + 1]
+                if batch["labels"] is not None:
+                    batch["labels"] = batch["labels"][:, : last_nonzero_indices + 1]
 
         output = super().eval_forward(batch, outputs)
 
         # Add back the 0 that we removed
-        if num_zeros: 
-            if hasattr(output, 'logits') or isinstance(output, dict):
-                fake_logits = torch.zeros(output['logits'].shape[0], num_zeros, output['logits'].shape[2], device=output['logits'].device)
+        if num_zeros:
+            if hasattr(output, "logits") or isinstance(output, dict):
+                fake_logits = torch.zeros(
+                    output["logits"].shape[0], num_zeros, output["logits"].shape[2], device=output["logits"].device
+                )
                 fake_logits[:, :, 0] = 1
-                output['logits'] = torch.cat([output['logits'], fake_logits], dim=1)
+                output["logits"] = torch.cat([output["logits"], fake_logits], dim=1)
             else:
                 fake_logits = torch.zeros(output.shape[0], num_zeros, output.shape[2], device=output.device)
                 fake_logits[:, :, 0] = 1
