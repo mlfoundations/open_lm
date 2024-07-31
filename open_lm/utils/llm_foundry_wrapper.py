a3896c5b91b802f7ec0d7d5b109b1715e124e187
Jean Mercat <jean.mercat@tri.global>
2024-07-30 20:22:34 -0700

    use super instead of copy paste

--- open_lm/utils/llm_foundry_wrapper.py
+++ open_lm/utils/llm_foundry_wrapper.py
@@ -55,7 +55,6 @@ class SimpleComposerOpenLMCausalLM(HuggingFaceModel):
         return super().generate(input_ids=input_ids, **kwargs)
     
     def eval_forward(self, batch, outputs: Optional[Any] = None):
-
         # If input_ids are all 0 after a certain point, we can skip that
         num_zeros = 0
         if batch['input_ids'] is not None:
@@ -69,72 +68,7 @@ class SimpleComposerOpenLMCausalLM(HuggingFaceModel):
                 if batch['labels'] is not None:
                     batch['labels'] = batch['labels'][:, : last_nonzero_indices + 1]
 
-        # If the batch mode is generate, we will generate a requested number of tokens using the underlying
-        # model's generate function. Extra generation kwargs can be passed in via the batch. Strings will
-        # be returned from eval_forward
-        if batch.get('mode', None) == 'generate':
-            if self.tokenizer is None:
-                raise ValueError(
-                    'Generation eval cannot be used without providing a tokenizer to the model constructor.',
-                )
-
-            self.labels = batch.pop('labels')
-            generation = self.generate(
-                batch['input_ids'],
-                attention_mask=batch['attention_mask'],
-                synced_gpus=dist.get_world_size() > 1,
-                **batch.get('generation_kwargs', {}),
-            )
-
-            # don't remove prefix space to sentencepiece models
-            if len(
-                self.tokenizer(' a', add_special_tokens=False)['input_ids'],  # pyright: ignore[reportGeneralTypeIssues]
-            ) == 1:
-                return self.tokenizer.batch_decode(
-                    generation[:, batch['input_ids'].shape[1]:],
-                    skip_special_tokens=True,
-                )
-            else:
-                return [
-                    ' ' + generation for generation in
-                    self.tokenizer.batch_decode(generation[:, batch['input_ids'].shape[1]:], skip_special_tokens=True)
-                ]
-
-        if self.use_logits or batch.get('mode', None) == 'icl_task':
-            # pop labels first to avoid computing loss
-            self.labels = batch.pop('labels')
-
-            # HF encoder decoder models like T5 expect either decoder_input_ids or labels,
-            # so we add decoder_input_ids to the batch if it is missing
-            if self.config.is_encoder_decoder and 'decoder_input_ids' not in batch:
-                if hasattr(self.model, 'prepare_decoder_input_ids_from_labels'):
-                    batch['decoder_input_ids'] = self.model.prepare_decoder_input_ids_from_labels(labels=self.labels)
-                else:
-                    raise RuntimeError(
-                        'Encoder decoder models require that either decoder_input_ids is present in the batch'
-                        ' or that the model has a prepare_decoder_input_ids_from_labels method.',
-                    )
-
-            if self.shift_labels or batch.get('mode', None) == 'icl_task':
-                assert self.labels is not None
-                # HF CausalLM models internally shift labels before computing loss, so we do the same here
-                self.labels[:, :-1] = self.labels[:, 1:].clone()
-                self.labels[:, -1] = -100
-
-            output = outputs if outputs else self.forward(batch)
-
-            if self.config.use_return_dict:
-                output = output['logits']
-            else:
-                # if loss was computed (cached outputs from forward), loss is at index 0 and logits are at index 1
-                # if loss was not computed (no cached outputs during eval), loss is not present and logits are at index 0
-                output = output[1] if len(output[0].shape) == 0 else output[0]
-
-            # if we are in the single class case, then remove the classes dimension
-            if output.ndim == 2 and output.shape[1] == 1:
-                output = output.squeeze(dim=1)
-        else:
-            output = outputs if outputs else self.forward(batch)
+        output = super().eval_forward(batch, outputs)
 
         # Add back the 0 that we removed
         if num_zeros: 
